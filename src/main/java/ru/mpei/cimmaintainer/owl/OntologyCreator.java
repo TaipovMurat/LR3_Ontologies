1b7b1cf394120d9ab69134158ca41bd4937b6758
Taipov Murat <TaipovMI@mpei.ru>
2023-05-16 17:28:25 +0300

    Update ontologie

--- src/main/java/ru/mpei/cimmaintainer/owl/OntologyCreator.java
+++ src/main/java/ru/mpei/cimmaintainer/owl/OntologyCreator.java
@@ -1,11 +1,6 @@
 package ru.mpei.cimmaintainer.owl;
 
-import org.apache.jena.ontology.DatatypeProperty;
-import org.apache.jena.ontology.Individual;
-import org.apache.jena.ontology.ObjectProperty;
-import org.apache.jena.ontology.OntClass;
-import org.apache.jena.ontology.OntModel;
-import org.apache.jena.ontology.OntProperty;
+import org.apache.jena.ontology.*;
 import org.apache.jena.rdf.model.Model;
 import org.apache.jena.rdf.model.ModelFactory;
 
@@ -52,7 +47,7 @@ public class OntologyCreator {
                                 "mode",
                                 "winding",
                                 "protection"
-                                ),
+                        ),
                         List.of("have")
                 ),
                 List.of(
@@ -61,15 +56,72 @@ public class OntologyCreator {
                         new Protection("МТЗ_ВН", List.of("installed_in")),
                         new Protection("ДЗ,_ТНЗНП", List.of("installed_in")),
                         new Protection("ТЗНП", List.of("installed_in"))
+                ),
+                new StationObject("ЛЭП",
+                        List.of(
+                                "lineVoltage",
+                                "protection"
+                        ),
+                        List.of("have")
+                ),
+                List.of(
+                        new Protection("ДЗЛ", List.of("installed_in")),
+                        new Protection("ДЗ", List.of("installed_in")),
+                        new Protection("ТЗНП", List.of("installed_in")),
+                        new Protection("МТЗ", List.of("installed_in"))
+                ),
+                new StationObject("Выключатель",
+                        List.of(
+                                "breakerVoltage",
+                                "protection"
+                        ),
+                        List.of("have")
+                ),
+                List.of(
+                        new Protection("МТЗ_С_ПОН", List.of("installed_in")),
+                        new Protection("МТЗ", List.of("installed_in")),
+                        new Protection("АВР", List.of("installed_in")),
+                        new Protection("ТО", List.of("installed_in")),
+                        new Protection("ЗОЗЗ", List.of("installed_in")),
+                        new Protection("ДЗ", List.of("installed_in"))
+                ),
+                new StationObject("Ошиновка",
+                        List.of(
+                                "isReactor",
+                                "protection"
+                        ),
+                        List.of("have")
+                ),
+                List.of(
+                        new Protection("ДЗО", List.of("installed_in"))
                 )
         );
+
     }
 
-    private static Map<String, String> objectsNames() {
+    private static Map<String, String> transfNames() {
         return Map.of(
                 "ТДЦ-150000_220/110", "ИЭУ-1",
-                "ДЦ-63000_110/10", "ИЭУ-2"
+                "ДЦ-63000_110/10", "ИЭУ-2",
+                "ТДЦ-160000_330/220", "ИЭУ-4"
         );
+    }
+    private static Map<String, String> lineNames() {
+        return Map.of(
+                "ЛЭП-1_110", "ИЭУ-3"
+        );
+    }
+    private static Map<String, String> breakerNames() {
+        return Map.of(
+                "BRK-1_6", "ИЭУ-6",
+                "BRK-2_35", "ИЭУ-7",
+                "BRK-3_35", "ИЭУ-8"
+        );
+    }
+    private static Map<String, String> busNames() {
+        return Map.of(
+                "BUS-1_1", "ИЭУ-9"
+                );
     }
 
     private static OntModel createModel() {
@@ -93,6 +145,7 @@ public class OntologyCreator {
             classes.put(ontClass, subClassesList);
         }
         return classes;
+
     }
 
     /**
@@ -141,10 +194,11 @@ public class OntologyCreator {
                 case "Трансформатор":
                     List<Individual> transformerIndividuals = new ArrayList<>();
                     List<Individual> protectionIndividuals = new ArrayList<>();
-                    Map<String, String> names = objectsNames();
+                    Map<String, String> names = transfNames();
 
                     for (Map.Entry<String, String> name : names.entrySet()) {
                         Individual transfIndividual = object.getKey().createIndividual(nameSpace + name.getKey());
+                        System.out.println(object.getKey().getLocalName());
                         transformerIndividuals.add(transfIndividual);
                         for (OntClass protection : object.getValue()) {
                             Individual protIndividual = protection.createIndividual(nameSpace + name.getValue());
@@ -153,6 +207,59 @@ public class OntologyCreator {
                     }
                     objectIndividuals.put(transformerIndividuals, protectionIndividuals);
                     break;
+
+                case "ЛЭП":
+                    List<Individual> lineIndividuals = new ArrayList<>();
+                    List<Individual> protectionlineIndividuals = new ArrayList<>();
+                    Map<String, String> lineNames = lineNames();
+
+                    for (Map.Entry<String, String> name : lineNames.entrySet()) {
+                        Individual lineIndividual = object.getKey().createIndividual(nameSpace + name.getKey());
+
+                        lineIndividuals.add(lineIndividual);
+                        for (OntClass protection : object.getValue()) {
+                            Individual protIndividual = protection.createIndividual(nameSpace + name.getValue());
+                            protectionlineIndividuals.add(protIndividual);
+                        }
+                    }
+
+                    objectIndividuals.put(lineIndividuals, protectionlineIndividuals);
+                    break;
+                case "Выключатель":
+                    List<Individual> brkIndividuals = new ArrayList<>();
+                    List<Individual> protectionBRKIndividuals = new ArrayList<>();
+                    Map<String, String> brkNames = breakerNames();
+
+                    for (Map.Entry<String, String> name : brkNames.entrySet()) {
+                        Individual lineIndividual = object.getKey().createIndividual(nameSpace + name.getKey());
+
+                        brkIndividuals.add(lineIndividual);
+                        for (OntClass protection : object.getValue()) {
+                            Individual protIndividual = protection.createIndividual(nameSpace + name.getValue());
+                            protectionBRKIndividuals.add(protIndividual);
+                        }
+                    }
+
+                    objectIndividuals.put(brkIndividuals, protectionBRKIndividuals);
+                   break;
+                case "Ошиновка":
+                    List<Individual> busIndividuals = new ArrayList<>();
+                    List<Individual> protectionBUSIndividuals = new ArrayList<>();
+                    Map<String, String> busNames = busNames();
+
+                    for (Map.Entry<String, String> name : busNames.entrySet()) {
+                        Individual lineIndividual = object.getKey().createIndividual(nameSpace + name.getKey());
+
+                        busIndividuals.add(lineIndividual);
+                        for (OntClass protection : object.getValue()) {
+                            Individual protIndividual = protection.createIndividual(nameSpace + name.getValue());
+                            protectionBUSIndividuals.add(protIndividual);
+                        }
+                    }
+
+                    objectIndividuals.put(busIndividuals, protectionBUSIndividuals);
+                    break;
+
             }
         }
         return objectIndividuals;
@@ -187,15 +294,73 @@ public class OntologyCreator {
                     DatatypeProperty mode = (DatatypeProperty) properties.get(0).get("mode");
                     objInd.addProperty(mode, "single");
                     DatatypeProperty winding = (DatatypeProperty) properties.get(0).get("winding");
-                    objInd.addProperty(winding, "2");
+                    objInd.addProperty(winding, "3");
                 }
                 DatatypeProperty power = (DatatypeProperty) properties.get(0).get("power");
-                if (objInd.getProperty(power) != null && objInd.getProperty(power).getDouble() < 160_000) {
+                DatatypeProperty voltage = (DatatypeProperty) properties.get(0).get("voltage");
+                DatatypeProperty winding = (DatatypeProperty) properties.get(0).get("winding");
+                if (objInd.getProperty(power) != null && objInd.getProperty(power).getDouble() >= 160_000
+                        && (objInd.getProperty(voltage).getDouble() >= 110)) {
+                    DatatypeProperty protection1 = (DatatypeProperty) properties.get(0).get("protection");
+                    objInd.addProperty(protection1, "Первый_комплект_ДЗТ");
+                    DatatypeProperty protection2 = (DatatypeProperty) properties.get(0).get("protection");
+                    objInd.addProperty(protection2, "Второй_комплект_ДЗТ");
+                } else if (objInd.getProperty(power) != null && objInd.getProperty(power).getDouble() < 160_000
+                        && (objInd.getProperty(voltage).getDouble() > 35 && objInd.getProperty(voltage).getDouble() < 110))
+                {
                     DatatypeProperty protection = (DatatypeProperty) properties.get(0).get("protection");
-                    objInd.addProperty(protection, "ДЗТ");
+                    objInd.addProperty(protection,"Один_комплект_ДЗТ");
+                }
+                if (objInd.getProperty(power) != null && objInd.getProperty(winding).getDouble() >= 3 ) {
+                    DatatypeProperty protection = (DatatypeProperty) properties.get(0).get("protection");
+                    objInd.addProperty(protection,"Защита от перегрузки");
+                }
+                if ("ЛЭП".equals(objInd.getOntClass().getLocalName())) {
+                    String lineVoltageStr = objInd.getURI().split("#")[1].split("_")[1].split("/")[0];
+                    DatatypeProperty lineVoltage = (DatatypeProperty) properties.get(0).get("lineVoltage");
+                    objInd.addProperty(lineVoltage, lineVoltageStr);
+                }
+                DatatypeProperty lineVoltage = (DatatypeProperty) properties.get(0).get("lineVoltage");
+                if (objInd.getProperty(lineVoltage) != null && objInd.getProperty(lineVoltage).getDouble() >= 110
+                        && objInd.getProperty(lineVoltage).getDouble() <= 220) {
+                    DatatypeProperty protection1 = (DatatypeProperty) properties.get(0).get("protection");
+                    objInd.addProperty(protection1, "ДЗЛ");
+                    DatatypeProperty protection2 = (DatatypeProperty) properties.get(0).get("protection");
+                    objInd.addProperty(protection2, "ДЗ");
+                    DatatypeProperty protection3 = (DatatypeProperty) properties.get(0).get("protection");
+                    objInd.addProperty(protection3,"ТЗНП");
+                    DatatypeProperty protection4 = (DatatypeProperty) properties.get(0).get("protection");
+                    objInd.addProperty(protection4,"АПВ");
+                    DatatypeProperty protection5 = (DatatypeProperty) properties.get(0).get("protection");
+                    objInd.addProperty(protection5,"ТО");
+                    DatatypeProperty protection6 = (DatatypeProperty) properties.get(0).get("protection");
+                    objInd.addProperty(protection6,"МТЗ");
+                }
+                if ("Выключатель".equals(objInd.getOntClass().getLocalName())) {
+                    String breakerVoltageStr = objInd.getURI().split("#")[1].split("_")[1].split("/")[0];
+                    DatatypeProperty breakerVoltage = (DatatypeProperty) properties.get(0).get("breakerVoltage");
+                    objInd.addProperty(breakerVoltage, breakerVoltageStr);
+                }
+                DatatypeProperty breakerVoltage = (DatatypeProperty) properties.get(0).get("breakerVoltage");
+                if (objInd.getProperty(breakerVoltage) != null && objInd.getProperty(breakerVoltage).getDouble() >= 6
+                        && objInd.getProperty(breakerVoltage).getDouble() <= 35) {
+                    DatatypeProperty protection1 = (DatatypeProperty) properties.get(0).get("protection");
+                    objInd.addProperty(protection1, "МТЗ");
+                }
+                if ("Ошиновка".equals(objInd.getOntClass().getLocalName())) {
+                    String numberOfReactorsStr = objInd.getURI().split("#")[1].split("_")[1].split("/")[0];
+                    DatatypeProperty numberOfReactors = (DatatypeProperty) properties.get(0).get("isReactor");
+                    objInd.addProperty(numberOfReactors, numberOfReactorsStr);
+                }
+                DatatypeProperty numberOfReactors = (DatatypeProperty) properties.get(0).get("isReactor");
+                if (objInd.getProperty(numberOfReactors) != null &&
+                        objInd.getProperty(numberOfReactors).getDouble() >= 1) {
+                    DatatypeProperty protection1 = (DatatypeProperty) properties.get(0).get("protection");
+                    objInd.addProperty(protection1, "ДЗО");
                 }
             }
         }
-    }
 
+
+    }
 }
